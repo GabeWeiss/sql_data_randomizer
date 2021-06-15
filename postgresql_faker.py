26561ad675319da140d5fd46c0bf8d7bc37579f4
Gabe Weiss <gweiss@google.com>
2021-06-14 17:32:56 -0700

    Changing over to use argparse instead of getops.

--- postgresql_faker.py
+++ postgresql_faker.py
@@ -1,68 +1,50 @@
 from faker import Faker
 
-import getopt
+import argparse
 import os
 import psycopg2
 import re
 import sys
 import time
 
+parser = argparse.ArgumentParser(description='This is a script that determines if a set of cameras in Eve Echoes is displaying a safe path or not.')
+parser.add_argument("-H", "--host", help="Specify a host destination for the PostgreSQL instance. Defaults to localhost.", default="127.0.0.1")
+parser.add_argument("-P", "--port", help="Specify a port for the PostgreSQL connection. Defaults to 5432.", default=5432)
+parser.add_argument("-u", "--user", help="Specify a database user.")
+parser.add_argument("-p", "--password", help="Specify the db user's password.")
+parser.add_argument("-d", "--dbname", help="Specify a database name to use in your PostgreSQL instance.")
+parser.add_argument("-l", "--locations", help="Specify number of locations for the fake employee data.", default=8)
+parser.add_argument("-e", "--employees", help="Specify number of employees per location for the fake employee data.", default=8)
+parser.add_argument("-a", "--auto", help="If the specified database name doesn't exist, passing this flag tells script to automatically create it.", action='store_true')
+parser.add_argument("-n", "--dontclean", help="By default the script truncates the tables. Passing this flag means the tables will be left alone.", action='store_true')
+parser.add_argument("-c", "--continuous", help="Instead of populating the db as fast as possible, this tells the script to stream instead up to the numbers specified.", action='store_true')
+args = parser.parse_args()
+
 # from config import SQL instance connection info, and 
 # our database information to connect to the db
-SQL_HOST = os.environ.get("SQL_HOST", "127.0.0.1") # Defaults to using localhost/Cloud SQL Proxy
-DB_PORT  = os.environ.get("DB_PORT", 5432)
-DB_USER  = os.environ.get("DB_USER", None)
-DB_PASS  = os.environ.get("DB_PASS", None)
-DB_NAME  = os.environ.get("DB_NAME", None)
+SQL_HOST = os.environ.get("SQL_HOST", args.host)
+DB_PORT  = os.environ.get("DB_PORT", args.port)
+DB_USER  = os.environ.get("DB_USER", args.user)
+DB_PASS  = os.environ.get("DB_PASS", args.password)
+DB_NAME  = os.environ.get("DB_NAME", args.dbname)
 
 # configurable defaults for how many variations you want
-LOCATIONS = 8
-EMPLOYEES = 8 # This is number of employees per location, not total
+LOCATIONS = args.locations
+# This is number of employees per location, not total
+EMPLOYEES = args.employees
 
 # parsing/handling commandline options
 auto_create = False
+if args.auto:
+    auto_create = True
 create_db = False
 
 # Note: By default, each time you run this script, it cleans the tables out
 # If you want to add more data instead of starting fresh, you can pass the flag '-c'
 # and it won't clean out the database, but will just add more random values to it
 clean_table = True
-fullCmdArguments = sys.argv
-argumentList = fullCmdArguments[1:]
-unixOptions = "hH:P:u:p:d:l:e:ac"
-gnuOptions = ["help", "host=", "port=", "user=", "passwd=", "dbname=", "locations=", "employees=", "auto", "dontclean"]
-
-# probably don't NEED to do all this try/catch, but makes it easier to catch what/where goes wrong sometimes
-# this chunk is just handling arguments
-try:
-    arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
-except getopt.error as err:
-    print (str(err))
-    sys.exit(2)
-
-for currentArgument, currentValue in arguments:
-    if currentArgument in ("-h", "--help"):
-        print ("\nusage: python postgresql_faker.py [-h | -P port | -u user | -p passwd | -d dbname | -l locations | -e employees]\nOptions and arguments (and corresponding environment variables):\n-d db\t: database name to connect to or create if it doesn't exist\n-e emps\t: number of employees per location to create\n-h\t: display this help\n-H addr\t: target MySQL database address. Defaults to 127.0.0.1\n-l locs\t: number of locations to create\n-P port\t: port to connect to\n-p pwd\t: password for the database user\n-u usr\t: database user to connect with\n-a\t: automatically create the database if it's missing\n-c\t: DON'T clean out the tables before inserting new random data. Default is to start clean\n\nOther environment variables:\nDB_USER\t: database user to connect with. Overridden by the -u flag\nDB_PASS\t: database password. Overridden by the -p flag.\nDB_NAME\t: database to connect to. Overridden by the -d flag.\nSQL_HOST: Remote MySQL database address. Overridden by the -H flag.\nDB_PORT\t: port for MySQL instance. Overridden by the -P flag.")
-        sys.exit(0)
-
-    if currentArgument in ("-H", "--host"):
-        SQL_HOST = currentValue
-    elif currentArgument in ("-P", "--port"):
-        DB_PORT = currentValue
-    elif currentArgument in ("-u", "--user"):
-        DB_USER = currentValue
-    elif currentArgument in ("-p", "--passwd"):
-        DB_PASS = currentValue
-    elif currentArgument in ("-d", "--dbname"):
-        DB_NAME = currentValue
-    elif currentArgument in ("-l", "--locations"):
-        LOCATIONS = int(currentValue)
-    elif currentArgument in ("-e", "--employees"):
-        EMPLOYEES = int(currentValue)
-    elif currentArgument in ("-a", "--auto"):
-        auto_create = True
-    elif currentArgument in ("-c", "--dontclean"):
-        clean_table = False
+if args.dontclean:
+    clean_table = False
 
 # Make sure that we have all the pieces we must have in order to connect to our db properly
 if not DB_USER:
@@ -101,23 +83,37 @@ def connect_database():
         )
     except (Exception, psycopg2.DatabaseError) as e:
         if re.search(f'FATAL\:[\s]+database \"{DB_NAME}\" does not exist\n', str(e)):
-            try:
-                mydb = psycopg2.connect(
-                    host=SQL_HOST,
-                    user=DB_USER,
-                    password=DB_PASS,
-                    port=DB_PORT
-                )
-                mydb.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-                cur = mydb.cursor()
-                cur.execute(f"CREATE DATABASE {DB_NAME}")
-                mydb.commit()
-                cur.close()
-                mydb.close()
-                mydb = None
-            except Exception as e:
-                print(e)
+            if auto_create == False:
+                u_input = input("Your database doesn't exist, would you like to create it (Y/n)? ")
+                if u_input == "Y":
+                    create_db = True
+                else:
+                    print ("The database doesn't exist and you've chosen to not create it.")
+                    sys.exit(0)
+            else:
+                create_db = True
+
+            if create_db:
+                try:
+                    mydb = psycopg2.connect(
+                        host=SQL_HOST,
+                        user=DB_USER,
+                        password=DB_PASS,
+                        port=DB_PORT
+                    )
+                    mydb.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+                    cur = mydb.cursor()
+                    cur.execute(f"CREATE DATABASE {DB_NAME}")
+                    mydb.commit()
+                    cur.close()
+                    mydb.close()
+                    mydb = None
+                except Exception as e:
+                    print(e)
+                    sys.exit(1)
+            else:
                 sys.exit(1)
+
 
             # Return here, which will exit into our connection loop, see db
             # is still none, and re-try, but now we've created our DB
@@ -196,6 +192,9 @@ def create_employees():
                 mycursor.execute(sql_command)
             except (Exception, psycopg2.DatabaseError) as e:
                 print (e)
+
+            if args.continuous:
+                time.sleep(0.5)
 
         mydb.commit()
 
